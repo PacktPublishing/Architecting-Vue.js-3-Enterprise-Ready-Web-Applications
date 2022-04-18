95a332dc44be3c59e7d6eb5891b6a22a2d40bcd8
Solomon Eseme <kaperskyguru@gmail.com>
2022-04-18 12:28:39 +0100

    feat: add internationalization and atomic design pattern

---
+++ src/locales/index.js
@@ -0,0 +1,11 @@
+import en from "./en.json";
+import fr from "./fr.json";
+import de from "./de.json";
+
+const messages = {
+  en,
+  fr,
+  de,
+};
+
+export default messages;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import App from "./App.vue";
+import locales from "./locales/index.js";
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  locale: "en", // set locale
+  fallbackLocale: "en", // set fallback locale
+  messages: locales, // set locale messages
+});
+
+const app = createApp(App);
+
+app.use(i18n);
+app.mount("#app");
