8430b7afd208cdda3c79affeb4dfb06954630a63
Solomon Eseme <kaperskyguru@gmail.com>
2022-05-22 14:51:49 +0100

    feat: graphql apollo config for vue 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import { createI18n } from "vue-i18n";
 import App from "./App.vue";
 import locales from "./locales/index.js";
+import apolloClient from "./plugins/apollo.config";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
 const i18n = createI18n({
   locale: "en", // set locale
@@ -9,7 +11,12 @@ const i18n = createI18n({
   messages: locales, // set locale messages
 });
 
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
 
 app.use(i18n);
 app.mount("#app");
