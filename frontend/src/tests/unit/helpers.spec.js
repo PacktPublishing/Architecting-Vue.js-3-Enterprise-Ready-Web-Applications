f72280aa57c4311222e94366b2bfe6e33d983264
Solomon Eseme <kaperskyguru@gmail.com>
2022-08-22 20:31:35 +0100

    feat: add component testing

--- frontend/src/tests/unit/helpers.spec.js
+++ frontend/src/tests/unit/helpers.spec.js
@@ -1,15 +1,16 @@
+import { describe, it, expect } from 'vitest';
 import { increment } from '../../helpers';
 
 describe('increment', () => {
-  test('increments the current number by 1', () => {
+  it('increments the current number by 1', () => {
     expect(increment(0, 10)).toBe(1);
   });
 
-  test('does not increment the current number over the max', () => {
+  it('does not increment the current number over the max', () => {
     expect(increment(10, 10)).toBe(10);
   });
 
-  test('has a default max of 10', () => {
+  it('has a default max of 10', () => {
     expect(increment(10)).toBe(10);
   });
 });
