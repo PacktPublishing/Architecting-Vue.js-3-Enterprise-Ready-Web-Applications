0400e8eec8b18afb05bfa8e12e66d0e36f1ac8a3
Solomon Eseme <kaperskyguru@gmail.com>
2022-08-22 16:00:07 +0100

    feat: add test using second method

--- frontend/src/components/atoms/Button.stories.js
+++ frontend/src/components/atoms/Button.stories.js
@@ -1,4 +1,4 @@
-import MyButton from '../Button.vue';
+import MyButton from './Button.vue';
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
--- frontend/src/components/organisms/Header.vue
+++ frontend/src/components/organisms/Header.vue
@@ -205,7 +205,7 @@
 </template>
 
 <script setup>
-import Button from '../atoms/Button/Button.vue';
+import Button from '../atoms/Button.vue';
 import SearchField from '../molecules/SearchField.vue';
 </script>
 <style scoped>
