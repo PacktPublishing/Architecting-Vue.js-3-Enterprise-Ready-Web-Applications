f3276c835238112c4cc9f99419e8604836a93bf6
Solomon Eseme <kaperskyguru@gmail.com>
2022-12-31 00:27:43 +0100

    feat: add e2e with cypress

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full">
+    <LoginOverview />
+  </div>
+</template>
+  
+  <script setup >
+import LoginOverview from "@/components/templates/LoginOverview.vue";
+</script>
+  
