3fda56b98b0674f5960a8ea157f6906cb9502eab
Solomon Eseme <kaperskyguru@gmail.com>
2022-12-31 00:58:34 +0100

    fix: add register

---
+++ frontend/src/components/templates/RegisterOverview.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <section class="mt-20">
+      <Register />
+    </section>
+  </main>
+</template>
+  
+  <script setup>
+import Register from '@/components/organisms/Register.vue';
+</script>
+  
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-full">
+    <RegisterOverview />
+  </div>
+</template>
+    
+<script setup>
+import RegisterOverview from '@/components/templates/RegisterOverview.vue';
+</script>
+    
