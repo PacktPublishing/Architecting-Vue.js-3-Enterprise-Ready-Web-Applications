1491d45cb0f2937f1acb203f203ae9ed45776195
Solomon Eseme <kaperskyguru@gmail.com>
2023-03-03 10:36:08 +0100

    fix: eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    "plugin:vue/vue3-recommended",
+    // 'plugin:vue/vue3-essential', // This option doesn't impose formatting rules
+    // 'plugin:vue/vue3-strongly-recommended', // This option imposes formatting rules on your code to improve readability
+  ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
